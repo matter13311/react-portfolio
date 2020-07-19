ae0d4ec0f71cce67ec800165cbed450774ef7143
matter13311 <matthewjack1331@yahoo.com>
2020-07-19 15:32:58 -0400

    Updates

---
+++ precache-manifest.564f76adfb6b2f5236cc5ba3d923bd4b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076c944e1a959ad8051e9f1bb3f428af",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3856aa0674be8956e2b2",
+    "url": "/react-portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "bb9844e431873cb59e15",
+    "url": "/react-portfolio/static/css/main.094e368c.chunk.css"
+  },
+  {
+    "revision": "3856aa0674be8956e2b2",
+    "url": "/react-portfolio/static/js/2.e110a3f4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.e110a3f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb9844e431873cb59e15",
+    "url": "/react-portfolio/static/js/main.6a0ae0fe.chunk.js"
+  },
+  {
+    "revision": "1f06fa0f70fd8f61fc2f",
+    "url": "/react-portfolio/static/js/runtime-main.646270ce.js"
+  },
+  {
+    "revision": "d2b35c0c3248835d8afb14e49fdf697a",
+    "url": "/react-portfolio/static/media/flags.d2b35c0c.png"
+  },
+  {
+    "revision": "3f466221a37c2c3848d67402881b7e59",
+    "url": "/react-portfolio/static/media/room.3f466221.png"
+  },
+  {
+    "revision": "92f18d535672ab76574cc7b2aebb0843",
+    "url": "/react-portfolio/static/media/shopping.92f18d53.jpg"
+  },
+  {
+    "revision": "eaa505396dde9f4fb1df6e1d789f9a50",
+    "url": "/react-portfolio/static/media/smiley.eaa50539.png"
+  }
+]);
